1bc1ed640cc61592e855618b05795fcb3d992acc
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-09-08 16:40:02 -0500

    feat(rama-proyecto): Creacion de los arreglos Productos, Precios y Cantidades vendidas

---
+++ src/main/java/edu/unicartagena/estructuras/proyecto/simuladorDeVentas.java
@@ -0,0 +1,24 @@
+package edu.unicartagena.estructuras.proyecto;
+
+/**
+ *
+ * @author STEVEN AF
+ */
+public class simuladorDeVentas {
+    public static void ejecutar() {
+        System.out.println("\n--- Proyecto: Simulador de Ventas ---");
+        
+        
+        // Productos
+        String productos[] = {"BolloLimpio", "Suero", "Queso", "Yuca", "Chicha"};
+        // Precios correspondientes
+        double precios[] = {1000, 2500, 25000, 2500, 1500};
+        // Ventas
+        int cantidades[] = {10, 5, 2, 5, 12};
+        
+
+    
+    }
+
+}
+
