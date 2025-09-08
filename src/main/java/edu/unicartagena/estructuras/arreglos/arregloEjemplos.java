e3cc470d9fa8186b8075d15dbf8a2e9e69582a87
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-09-08 10:51:48 -0500

    feat(arreglos): For clásico y for-each, impares a cero, valores por índice, búsqueda lineal implementada.

--- src/main/java/edu/unicartagena/estructuras/arreglos/arregloEjemplos.java
+++ src/main/java/edu/unicartagena/estructuras/arreglos/arregloEjemplos.java
@@ -2,6 +2,7 @@ package edu.unicartagena.estructuras.arreglos;
 
 import java.util.Random;
 
+
 /**
  *
  * @author STEVEN AF
@@ -9,15 +10,27 @@ import java.util.Random;
 public class arregloEjemplos {
     public static void ejecutar() {
         System.out.println("\n--- Arreglos ---");
-
         
         int[] numeros = new int[10];
         Random random = new Random();
-
         
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = random.nextInt(100);
+        for(int i =0; i < numeros.length; i++) {
+        numeros[i] = random.nextInt(100);
         }
+        
+        
+        
+        
+        System.out.println("Recorido usando for clasico");
+        
+        for(int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i] + "");
+        
+        }
+        
+        
+        
+         
     }
 }
 
